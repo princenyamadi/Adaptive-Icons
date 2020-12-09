3cc1764d5368c70f12e1d193ac810d67c6d8991b
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-09 17:39:51 +0000

    Working with autocomplete textview

--- app/src/main/java/tech/peny/adaptiveicons/MainActivity.java
+++ app/src/main/java/tech/peny/adaptiveicons/MainActivity.java
@@ -3,6 +3,8 @@ package tech.peny.adaptiveicons;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+        String[] friends = {"Nick","Rick","Sarah","James","Nina","Noel","Sandra","Perpetual"};
+
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,friends);
+        autoCompleteTextView.setAdapter(arrayAdapter);
+        autoCompleteTextView.setThreshold(1);
+
+
+
+
     }
 }
